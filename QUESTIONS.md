6cb7f5aeac4d9262d68fd470c371297a65e410d0
Sophia Ciocca <sophiaciocca@gmail.com>
2017-06-07 14:52:36 -0400

    #4 seed db (#20)
    * Edit seed file for our actual models, not working yet
    
    * Fix seed file, working now for Product, User, and Categories
    
    * Return auth to original and update gitignore to ignore Sophias notes
    
    * Return auth to original and update gitignore to ignore Sophias notes

--- QUESTIONS.md
+++ QUESTIONS.md
@@ -1,3 +1,17 @@
 ##QUESTIONS
 
 1) How do we find what port we're on??????
+
+
+//NOTES FOR OAUTH SECRETS
+
+//secrets.js
+const {env} = require('APP');
+
+env.GITHUB_CLIENT_ID = 'ak;lsdjfla;sdjkf;asdjlkf;asdfsd;';
+
+then require it in at the START and gitignore it!!!
+
+//.gitignore
+
+add just "secrets.js" to .gitignore file
--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,17 @@
 'use strict'
 
 const db = require('APP/db')
-    , {User, Thing, Favorite, Promise} = db
+    , {User, Product, Category, Promise} = db
     , {mapValues} = require('lodash')
 
 function seedEverything() {
   const seeded = {
     users: users(),
-    things: things(),
+    products: products(),
+    categories: categories()
   }
 
-  seeded.favorites = favorites(seeded)
+  // seeded.favorites = favorites(seeded)
 
   return Promise.props(seeded)
 }
@@ -20,52 +21,81 @@ const users = seed(User, {
     email: 'god@example.com',
     name: 'So many names',
     password: '1234',
+    isAdmin: true
   },
   barack: {
     name: 'Barack Obama',
     email: 'barack@example.gov',
-    password: '1234'
+    password: '1234',
+    isAdmin: false
+  }
+})
+
+const products = seed(Product, {
+  galaxy: {
+    title: 'galaxy',
+    description: 'These are great galaxy leggings',
+    price: 100,
+    photoUrl: 'http://lorempixel.com/300/300/'
   },
+  snazzyworkout: {
+    title: 'snazzyworkout',
+    description: 'These are great snazzy workout leggings',
+    price: 35,
+    photoUrl: 'http://lorempixel.com/300/300/'
+  },
+  plainblack: {
+    title: 'plainblack',
+    description: 'These are great puppy leggings',
+    price: 20,
+    photoUrl: 'http://lorempixel.com/300/300/'
+  }
 })
 
-const things = seed(Thing, {
-  surfing: {name: 'surfing'},
-  smiting: {name: 'smiting'},
-  puppies: {name: 'puppies'},
+const categories = seed(Category, {
+  artistDesigned: {
+    title: 'artist-designed'
+  },
+  athletic: {
+    title: 'athletic'
+  },
+  basic: {
+    title: 'basic'
+  }
 })
 
-const favorites = seed(Favorite,
-  // We're specifying a function here, rather than just a rows object.
-  // Using a function lets us receive the previously-seeded rows (the seed
-  // function does this wiring for us).
-  //
-  // This lets us reference previously-created rows in order to create the join
-  // rows. We can reference them by the names we used above (which is why we used
-  // Objects above, rather than just arrays).
-  ({users, things}) => ({
-    // The easiest way to seed associations seems to be to just create rows
-    // in the join table.
-    'obama loves surfing': {
-      user_id: users.barack.id,    // users.barack is an instance of the User model
-                                   // that we created in the user seed above.
-                                   // The seed function wires the promises so that it'll
-                                   // have been created already.
-      thing_id: things.surfing.id  // Same thing for things.
-    },
-    'god is into smiting': {
-      user_id: users.god.id,
-      thing_id: things.smiting.id
-    },
-    'obama loves puppies': {
-      user_id: users.barack.id,
-      thing_id: things.puppies.id
-    },
-    'god loves puppies': {
-      user_id: users.god.id,
-      thing_id: things.puppies.id
-    },
-  })
-)
+// const favorites = seed(Favorite,
+//   // We're specifying a function here, rather than just a rows object.
+//   // Using a function lets us receive the previously-seeded rows (the seed
+//   // function does this wiring for us).
+//   //
+//   // This lets us reference previously-created rows in order to create the join
+//   // rows. We can reference them by the names we used above (which is why we used
+//   // Objects above, rather than just arrays).
+//   ({users, things}) => ({
+//     // The easiest way to seed associations seems to be to just create rows
+//     // in the join table.
+//     'obama loves surfing': {
+//       user_id: users.barack.id,    // users.barack is an instance of the User model
+//                                    // that we created in the user seed above.
+//                                    // The seed function wires the promises so that it'll
+//                                    // have been created already.
+//       thing_id: things.surfing.id  // Same thing for things.
+//     },
+//     'god is into smiting': {
+//       user_id: users.god.id,
+//       thing_id: things.smiting.id
+//     },
+//     'obama loves puppies': {
+//       user_id: users.barack.id,
+//       thing_id: things.puppies.id
+//     },
+//     'god loves puppies': {
+//       user_id: users.god.id,
+//       thing_id: things.puppies.id
+//     },
+//   })
+// )
 
 if (module === require.main) {
   db.didSync
@@ -135,4 +165,4 @@ function seed(Model, rows) {
   }
 }
 
-module.exports = Object.assign(seed, {users, things, favorites})
+module.exports = Object.assign(seed, {users, products, categories})
