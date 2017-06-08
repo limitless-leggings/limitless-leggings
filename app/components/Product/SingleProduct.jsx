89ddf6a26a06e4067465a1ec00214563150d0ae8
Marina <marinahoashi@gmail.com>
2017-06-08 17:23:42 -0400

    React bootstrap #23 (#29)
    * Add stylesheets and bootstrap.min.css files
    
    * Install react-bootstrap module
    
    * Add comment

--- app/components/Product/SingleProduct.jsx
+++ app/components/Product/SingleProduct.jsx
@@ -2,25 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { fetchProductById } from '../../redux/products'
+// import { ButtonToolbar, Button } from 'react-bootstrap'
+// Example of how to use components from React Bootstrap
 
 /* -----------------    COMPONENT     ------------------ */
 
-class SingleProduct extends React.Component {
-  render() {
-    const selectedProduct = this.props.selected || { title: null }
-
-    return (
-      <div>
-        <h1>This is the single-story page!</h1>
-        <h2>Current product selected: { selectedProduct.title }</h2>
-      </div>
-    )
-  }
-}
+const SingleProduct = ({selectedProduct}) => (
+  <div>
+    <h1>This is the single-product page!</h1>
+    <h2>Current product selected: { selectedProduct.title }</h2>
+  </div>
+)
 
 /* -----------------    CONTAINER     ------------------ */
 
-const mapStateToProps = ({ products }) => ({ selected: products.selected }) // getting selectedProduct off of State and passing it into dumb part of component as props.selectedProduct
+const mapStateToProps = ({ products }) => ({ selectedProduct: products.selected }) // getting selectedProduct off of State and passing it into dumb part of component as props.selectedProduct
 
 // const mapDispatch = { fetchProductById } // taking the fetchProductById dispatcher from the redux file, and
 
