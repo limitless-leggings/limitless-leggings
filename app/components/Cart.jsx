70fb7badc55c3ec3344e5ee4d9e6258a3df6d9a8
Yiling Jiang <yiling124@gmail.com>
2017-06-12 14:42:41 -0400

    cart finally work

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import { UPDATE_QUANTITY } from '../redux/cartItems'
+import { updateQty } from '../redux/cartItems'
 
 /* -----------------    COMPONENT     ------------------ */
 class Cart extends React.Component {
   constructor(props) {
     super(props)
-    const temp = {}
-    this.props.cart.map(cartItem => {
-      temp[cartItem.id] = cartItem.quantity
-    })
 
-    this.state = temp
+    this.state = {
+      cartItems: {}
+    }
+    // set total price as part of the state
 
     this.handleChange = this.handleChange.bind(this)
-    this.handleSbumit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   calculateTotal() {
@@ -27,6 +26,7 @@ class Cart extends React.Component {
 
   render() {
     const { cart } = this.props
+    console.log('~!~!~!~CART', cart)
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -34,52 +34,44 @@ class Cart extends React.Component {
             cart.map(cartItem => {
               const bindedHandleChange = this.handleChange.bind(this, cartItem.id)
               return (
-                <row key={cartItem.id}>
+                <div className="form-inline" key={cartItem.id}>
                   <h4><a href="">{cartItem.product.title}</a></h4>
                   <p>$ {cartItem.product.price}</p>
-                  <input placeholder={cartItem.quantity} onChange={bindedHandleChange} value= {this.state[cartItem.id]}></input>
-                  <button type='submit' value='submit' className="btn btn-info btn-sm">update</button>
-                </row>
+                  <input placeholder={cartItem.quantity} onChange={bindedHandleChange} value={this.state[cartItem.id]}></input>
+                </div>
               )
             })
           }
+          <button type='submit' value='submit' className="btn btn-info btn-sm">Update Cart</button>
         </form>
         <br></br>
         <span>Total Product Price: </span>
         <span className='text-right ng-binding'>${this.calculateTotal()}</span>
         <br></br>
-        <button className="btn btn-info btn-sm"><Link to="/">Checkout</Link></button>
+        <button><Link to="/">Checkout</Link></button>
       </div>
     )
   }
 
   handleChange(itemIndex, event) {
-    console.log('change event target!!!', event.target.value)
-    const temp = Object.assign({}, this.state)
+    const temp = Object.assign({}, this.state.cartItems)
     temp[itemIndex] = Number(event.target.value)
-    console.log('temp', temp)
-    this.setState(temp, () => console.log('state changed', this.state)
-    )
+    console.log('temppp', temp)
+    this.setState({cartItems: temp}, () => { console.log('hello') })
   }
 
   handleSubmit(event) {
     event.preventDefault()
-    // console.log('submit target!!!', event.target)
-    // this.props.increase()
+    const updatedValues = this.state.cartItems
+    this.props.updateQty(updatedValues)
   }
 }
 
 /* -----------------    CONTAINER     ------------------ */
 
 const mapStateToProps = ({ cart }) => ({ cart })
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increase: (item) => {
-      dispatch({
-        type: UPDATE_QUANTITY
-      })
-    }
-  }
-}
+
+const mapDispatchToProps ={updateQty}
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart)
--- app/redux/cartItems.jsx
+++ app/redux/cartItems.jsx
@@ -3,13 +3,13 @@ import axios from 'axios'
 /* -----------------    ACTIONS     ------------------ */
 
 const INITIALIZE = 'INITIALIZE_CARTITEMS'
-const UPDATE_QUANTITY = 'INCREASE_QUANTITY'
+const UPDATE = 'UPDATE_QUANTITY'
 // const REMOVE     = 'REMOVE_STORY';
 
 /* ------------   ACTION CREATORS     ------------------ */
 
 const init = cartItems => ({ type: INITIALIZE, cartItems })
-// const create = story   => ({ type: CREATE, story });
+const update = updatedValues => ({ type: UPDATE, updatedValues })
 // const remove = id      => ({ type: REMOVE, id });
 // const update = story   => ({ type: UPDATE, story });
 
@@ -20,16 +20,19 @@ export default function reducer(cartItems = [], action) {
   case INITIALIZE:
     return action.cartItems
 
-  case UPDATE_QUANTITY:
-    return //
-
-  // case REMOVE_USER:
-  //   return stories.filter(story => story.author_id !== action.id);
-
   // case UPDATE:
-  //   return stories.map(story => (
-  //     action.story.id === story.id ? action.story : story
-  //   ));
+  //   console.log('~!~!~!~!CART ITEMS', action.updatedValues)
+  //   return Object.assign({}, action.updatedValues)
+      // return cartItems.map(cartitem => (
+      //   action.cartItems.id === .id ? action.story :
+      // ))
+
+    // case REMOVE_USER:
+    //   return stories.filter(story => story.author_id !== action.id);
+
+    //   return stories.map(story => (
+    //     action.story.id === story.id ? action.story : story
+    //   ));
 
   default:
     return cartItems
@@ -39,11 +42,25 @@ export default function reducer(cartItems = [], action) {
 /* ------------       DISPATCHERS     ------------------ */
 
 export const fetchCartItems = () => dispatch => {
-  axios.get('/api/cart')
-    .then(res => dispatch(init(res.data)))
+  return axios.get('/api/cart')
+    .then(res => {
+      return dispatch(init(res.data))
+    })
     .catch(err => console.error('Fetching cartItems unsuccessful', err))
 }
 
-export const updateQty = () => dispatch => {
-  axios.put('/api/cartItems/')
+export const updateQty = (updatedValues) => dispatch => {
+  console.log('updatedValue', updatedValues)
+  const promisesArr= []
+  for (var key in updatedValues) {
+    const temp = {}
+    temp.id = Number(key)
+    temp.quantity = updatedValues[key]
+    promisesArr.push(axios.put(`/api/cart/${key}`, temp))
+  }
+  Promise.all(promisesArr)
+  .then(res => {
+    return dispatch(fetchCartItems())
+  })
+    .catch(err => console.error('Update cartItems unsuccessful', err))
 }
