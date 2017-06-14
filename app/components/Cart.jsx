3539a0ad4add6543ae4e43c249fa3d8eaee8594e
Yiling Jiang <yiling124@gmail.com>
2017-06-13 21:44:53 -0400

    updated (#85)

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -17,10 +17,11 @@ class Cart extends React.Component {
   }
 
   calculateTotal() {
-    if (this.props.cart.productItem) {
+    if (this.props.cart.length) {
       return this.props.cart.reduce((acc, item) => {
         return acc + (item.productItem.product.price * item.quantity)
       }, 0)
+      .toFixed(2)
     }
   }
 
@@ -28,28 +29,54 @@ class Cart extends React.Component {
     const { cart } = this.props
     return (
       <div>
+        <div className="row">
+           <div className="col-md-2 col-md-offset-1">
+             <h4>Products</h4>
+           </div>
+           <div className="col-md-2 col-md-offset-1">
+             <h4>Price</h4>
+           </div>
+           <div className="col-md-2 col-md-offset-1">
+             <h4>Quantity</h4>
+           </div>
+        </div>
         <form onSubmit={this.handleSubmit}>
           {
             cart.length > 0
-            ? cart.map(cartItem => {
-              const bindedHandleChange = this.handleChange.bind(this, cartItem.id)
-              return (
-                <div className="form-inline" key={cartItem.id}>
-                  <h4><Link to={`/products/${cartItem.id}`}>{cartItem.productItem && cartItem.productItem.product.title} - size {cartItem.productItem && cartItem.productItem.size}</Link></h4>
-                  <p>$ {cartItem.productItem && cartItem.productItem.product.price}</p>
-                  <input placeholder={cartItem.quantity} onChange={bindedHandleChange} value={this.state[cartItem.id]}></input>
-                </div>
-              )
-            })
-            : <h2>Your Cart is Empty!</h2>
+              ? cart.map(cartItem => {
+                const bindedHandleChange = this.handleChange.bind(this, cartItem.id)
+                return (
+                  <div className="row" key={cartItem.id}>
+                    <div className="col-md-3 col-md-offset-1">
+                      <h4><Link to={`/products/${cartItem.id}`}>{cartItem.productItem && cartItem.productItem.product.title} - size {cartItem.productItem && cartItem.productItem.size}</Link></h4>
+                    </div>
+                    <div className="col-md-3">
+                      <p>$ {cartItem.productItem && cartItem.productItem.product.price}</p>
+                    </div>
+
+                    <div className="col-md-3">
+                      <input placeholder={cartItem.quantity} onChange={bindedHandleChange} value={this.state[cartItem.id]}></input>
+                    </div>
+                    <thead>
+                    </thead>
+                  </div>
+                )
+              })
+              : <h2>Your Cart is Empty!</h2>
           }
-          <button type='submit' value='submit' className="btn btn-info btn-sm">Update Cart</button>
+          <div className="col-md-offset-1">
+            <button type='submit' value='submit' className="btn btn-info btn-sm">Update Cart</button>
+          </div>
         </form>
         <br></br>
-        <span>Total Product Price: </span>
-        <span className='text-right ng-binding'>${this.calculateTotal()}</span>
+        <div className="col-md-offset-1">
+          <span>Total Product Price: </span>
+          <span className='text-right ng-binding'>${this.calculateTotal()}</span>
+        </div>
         <br></br>
-        <button><Link to="/checkout">Checkout</Link></button>
+        <div className="col-md-offset-1">
+          <button><Link to="/checkout">Checkout</Link></button>
+        </div>
       </div>
     )
   }
@@ -57,7 +84,7 @@ class Cart extends React.Component {
   handleChange(itemIndex, event) {
     const temp = Object.assign({}, this.state.cartItems)
     temp[itemIndex] = Number(event.target.value)
-    this.setState({cartItems: temp})
+    this.setState({ cartItems: temp })
   }
 
   handleSubmit(event) {
